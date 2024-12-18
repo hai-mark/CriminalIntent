891d8b15e90e3559e699de36833485b7e9d8fdaf
hai-mark <errrorfour@gmail.com>
2024-12-19 01:43:27 +0700

    Настройка виджета EditText (CrimeFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -1,13 +1,17 @@
 package com.bignerdranch.android.criminalintent
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CrimeFragment: Fragment() {
 
     private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,8 @@ class CrimeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title) as EditText
+
         return view
     }
 }
