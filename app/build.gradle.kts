46e9b571cb9d3c8e6c6952532a3a5ce05d163e23
hai-mark <errrorfour@gmail.com>
2024-12-19 01:23:46 +0700

    Файл макета для представления фрагмента  (res/layout/fragment_crime.xml)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.bignerdranch.android.criminalintent"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bignerdranch.android.criminalintent"
