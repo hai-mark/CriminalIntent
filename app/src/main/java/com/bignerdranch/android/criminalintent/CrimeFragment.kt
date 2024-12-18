351dfd9e178adf0d705f4df44bb9fc4aaf93f0a1
hai-mark <errrorfour@gmail.com>
2024-12-19 01:40:36 +0700

    Переопределение onCreateView(...)  (CrimeFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.criminalintent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CrimeFragment: Fragment() {
@@ -9,5 +12,14 @@ class CrimeFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        return view
     }
 }
