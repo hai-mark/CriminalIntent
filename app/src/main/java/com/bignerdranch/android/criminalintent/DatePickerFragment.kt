74501e9fb217f06b674f97d2c4c6436c108032b4
hai-mark <errrorfour@gmail.com>
2024-12-24 03:12:37 +0700

    Возврат даты (DatePickerFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -3,9 +3,11 @@ package com.bignerdranch.android.criminalintent
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
 import java.util.Calendar
 import java.util.Date
+import java.util.GregorianCalendar
 
 private const val ARG_DATE = "date"
 class DatePickerFragment : DialogFragment() {
@@ -14,6 +16,17 @@ class DatePickerFragment : DialogFragment() {
     }
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
+        val dateListener =
+            DatePickerDialog.OnDateSetListener {
+                    _: DatePicker, year: Int,
+                    month: Int, day: Int ->
+                val resultDate : Date =
+                    GregorianCalendar(year, month, day).time
+                targetFragment?.let { fragment ->
+                    (fragment as
+                            Callbacks).onDateSelected(resultDate)
+                }
+            }
         val date = arguments?.getSerializable(ARG_DATE) as Date
         val calendar = Calendar.getInstance()
         calendar.time = date
@@ -25,7 +38,7 @@ class DatePickerFragment : DialogFragment() {
             calendar.get(Calendar.DAY_OF_MONTH)
         return DatePickerDialog(
             requireContext(),
-            null,
+            dateListener,
             initialYear,
             initialMonth,
             initialDay
