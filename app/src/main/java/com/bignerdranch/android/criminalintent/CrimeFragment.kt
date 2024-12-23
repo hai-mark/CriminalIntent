64f1e168295329faf424d8482506b46d831d236f
hai-mark <errrorfour@gmail.com>
2024-12-24 02:53:07 +0700

    Загрузка фрагмента CrimeFragment в  CrimeDetailViewModel (CrimeFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -10,6 +10,8 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import java.util.UUID
 
 private const val TAG = "CrimeFragment"
@@ -19,13 +21,16 @@ class CrimeFragment : Fragment() {
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
+    private val crimeDetailViewModel:
+            CrimeDetailViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeDetailViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
         val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
-        Log.d(TAG, "args bundle crime ID: $crimeId")
-// Загрузка преступления из базы данных
+
     }
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +50,19 @@ class CrimeFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View,
+                               savedInstanceState: Bundle?) {
+        super.onViewCreated(view,
+            savedInstanceState)
+        crimeDetailViewModel.crimeLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            })
+    }
     override fun onStart() {
         super.onStart()
         val titleWatcher = object : TextWatcher
@@ -79,6 +97,12 @@ class CrimeFragment : Fragment() {
         }
     }
 
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        solvedCheckBox.isChecked =
+            crime.isSolved
+    }
     companion object {
         fun newInstance(crimeId: UUID):
                 CrimeFragment {
