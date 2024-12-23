ec993ac014950329edc292695fe7ec430b96fefe
hai-mark <errrorfour@gmail.com>
2024-12-24 02:55:45 +0700

    Пропуск анимации флажком (CrimeFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -100,8 +100,10 @@ class CrimeFragment : Fragment() {
     private fun updateUI() {
         titleField.setText(crime.title)
         dateButton.text = crime.date.toString()
-        solvedCheckBox.isChecked =
-            crime.isSolved
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
     }
     companion object {
         fun newInstance(crimeId: UUID):
