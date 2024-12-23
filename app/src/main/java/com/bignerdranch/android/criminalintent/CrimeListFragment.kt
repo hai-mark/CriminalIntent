5a167879d6a245edc80f166b2b95d3285bdfeedf
hai-mark <errrorfour@gmail.com>
2024-12-24 01:11:21 +0700

    Начало ViewHolder (CrimeListFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,7 +32,13 @@ class CrimeListFragment : Fragment() {
             LinearLayoutManager(context)
         return view
     }
-
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView =
+            itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView =
+            itemView.findViewById(R.id.crime_date)
+    }
 
     companion object {
         fun newInstance(): CrimeListFragment {
