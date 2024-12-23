b3fc9144815314d78205d3e4c2bd9f304b4fdbc9
hai-mark <errrorfour@gmail.com>
2024-12-24 02:33:31 +0700

    Возврат LiveData из хранилища  (CrimeRepository.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.room.Room
 import database.CrimeDatabase
 import java.util.UUID
@@ -16,10 +17,8 @@ constructor(context: Context) {
             DATABASE_NAME
         ).build()
     private val crimeDao = database.crimeDao()
-    fun getCrimes(): List<Crime> =
-        crimeDao.getCrimes()
-    fun getCrime(id: UUID): Crime? =
-        crimeDao.getCrime(id)
+    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
+    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
 
     companion object {
         private var INSTANCE: CrimeRepository? =
