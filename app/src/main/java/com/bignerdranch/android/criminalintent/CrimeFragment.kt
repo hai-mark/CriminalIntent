830ba220322da22f89a4fa49f57d8f709277c8f1
hai-mark <errrorfour@gmail.com>
2024-12-24 02:49:28 +0700

    Получение идентификатора преступления из  аргументов (CrimeFragment.tk)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import java.util.UUID
 
+private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
 class CrimeFragment : Fragment() {
     private lateinit var crime: Crime
@@ -21,6 +23,9 @@ class CrimeFragment : Fragment() {
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        Log.d(TAG, "args bundle crime ID: $crimeId")
+// Загрузка преступления из базы данных
     }
     override fun onCreateView(
         inflater: LayoutInflater,
