6d2795303099ac384cf28868a47cc280828e9765
hai-mark <errrorfour@gmail.com>
2024-12-24 01:17:40 +0700

    Очистка элементов связанного списка

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -43,10 +43,16 @@ class CrimeListFragment : Fragment() {
 
     private inner class CrimeHolder(view: View)
         : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView =
-            itemView.findViewById(R.id.crime_title)
-        val dateTextView: TextView =
-            itemView.findViewById(R.id.crime_date)
+
+        private lateinit var crime: Crime
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
     }
 
     private inner class CrimeAdapter(var crimes: List<Crime>)
@@ -60,10 +66,7 @@ class CrimeListFragment : Fragment() {
         override fun getItemCount() = crimes.size
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+            holder.bind(crime)
         }
     }
 
